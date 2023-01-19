7c4d82312749dee29e4d6ec7b09c348af208862f
RajKumarhoc <115209391+RajKumarhoc@users.noreply.github.com>
2023-01-19 14:48:11 +0530

    Add files via upload

---
+++ Ping.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+class Ping
+{
+public static void main(String args [])throws Exception
+{
+String str;
+System.out.println("Enter the IP Address to be ping: ");
+BufferedReader buf1=new BufferedReader(new InputStreamReader(System.in));
+String ip=buf1.readLine();
+Runtime H=Runtime.getRuntime();
+Process p=H.exec("ping " + ip);
+InputStream in=p.getInputStream();
+BufferedReader buf2=new BufferedReader(new InputStreamReader(in));
+while((str=buf2.readLine())!=null)
+{
+System.out.println(" " + str);
+}
+}
+}
